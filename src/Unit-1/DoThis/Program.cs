aefe61bdba3a13f9b1b891a4829a86f62d88daf0
AlexandraDotlic <alexandra.nerandzic@gmail.com>
2022-10-19 09:09:01 +0200

    task 1 complete

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -6,12 +6,18 @@ namespace WinTail
     #region Program
     class Program
     {
+        /*NOTE: 
+         * When creating Props, ActorSystem, or ActorRef you will very rarely see the new keyword.
+         * These objects must be created through the factory methods built into Akka.NET.
+         * If you're using new you might be making a mistake.
+         */
         public static ActorSystem MyActorSystem;
 
         static void Main(string[] args)
         {
             // initialize MyActorSystem
             // YOU NEED TO FILL IN HERE
+            MyActorSystem = ActorSystem.Create("Myactorsystem");
 
             PrintInstructions();
 
@@ -19,11 +25,12 @@ namespace WinTail
             //YOU NEED TO FILL IN HERE
             // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
             // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
-
+            var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
+            var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
 
             // tell console reader to begin
             //YOU NEED TO FILL IN HERE
-
+            consoleReaderActor.Tell("start");
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
         }
